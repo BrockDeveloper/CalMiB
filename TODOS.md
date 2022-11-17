872407e13038f8f3817564e3151af8ba50bb9c03
DamianoPellegrini <damiano1.pellegrini@gmail.com>
2022-11-17 21:08:46 +0100

    feat: :sparkles: minimally viable product

---
+++ TODOS.md
@@ -0,0 +1,10 @@
+# TODOS
+
+- [x] Funzionamento base
+- [ ] LICENSE e README.md
+- [ ] Gestione Errori
+- [ ] Dividi in file
+- [ ] cache?
+- [ ] DOCKER?
+- [ ] Configurazione OpenAPI (Swagger)
+- [ ] Metti pubblicità al discord e cose
--- main.py
+++ main.py
@@ -1,22 +1,103 @@
-# /{corso}/{anno}/{anno_accademico}/{percorso}/?{lang}
+from fastapi import FastAPI, Path, Response, status
+from fastapi.responses import JSONResponse
+from enum import Enum
+from ics import Calendar, Event, DisplayAlarm
+from datetime import datetime, time, timedelta
+from dateutil import tz
+import requests
 
-# campi del calendario:
-# - lez/ese/tipo_short
-# - insegnamento
-# - percorso
-# - docente
-# - aula completa
+ENDPOINT = 'https://gestioneorari.didattica.unimib.it/PortaleStudentiUnimib/grid_call.php'
+ROME = tz.gettz('Europe/Rome')
 
-# campi da parsare: percorsi json [ricorda timezone UTC+1]
-# $ = $.celle
-# $.[].ora_inizio
-# $.[].ora_fine
-# $.[].data
-# $.[].tipo
-# $.[].display[]
-# $.[].{$.[].display.}
-# $.[].insegnamento_tipo[]
-# $.celle.*.tipo
-# $.[].docente
-# $.[].nome_insegnamento
-# $.[].codice_aula
+
+class Lang(str, Enum):
+    italian = 'it'
+    english = 'en'
+
+
+class Corso(str, Enum):
+    informatica_triennale = 'E3101Q'
+
+
+class AnnoAccademico(int, Enum):
+    primo = 1
+    secondo = 2
+    terzo = 3
+
+
+class Percorso(str, Enum):
+    unico = 'TO-DO'
+    t_uno = 'GGG_A-L'
+    t_due = 'GGG_M-Z'
+
+
+class ResponseMessage:
+    def __init__(self, message: str):
+        self.message = message
+
+
+app = FastAPI(
+    title='CalMiB',
+    version='0.0.1',
+    description='Calendar feed for unimib lessons',
+)
+
+
+@app.get('/{corso}/{anno}/{anno_accademico}/{percorso}',
+         responses={
+             '503': {'description': 'Service Unavailable'},
+         })
+async def root(
+    corso: Corso,
+    percorso: Percorso,
+    anno: int = Path(title="L'anno solare del calendario",
+                     ge=2020, le=datetime.utcnow().year),
+    anno_accademico: int = Path(
+        title="L'anno accademico di interesse", ge=1, le=3),
+    lang: Lang = Lang.italian,
+    alarms: bool = False,
+):
+
+    req = {
+        'view': 'easycourse',
+        'include': 'corso',
+        'all_events': 1,
+        '_lang': lang,
+        'anno': anno,
+        'anno2[]': f"{percorso}|{anno_accademico}",
+        'corso': corso,
+    }
+
+    try:
+        res = requests.post(ENDPOINT, data=req)
+    except:
+        return JSONResponse(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, content=ResponseMessage('Service Unavailable'))
+
+    data = res.json()
+    celle: list = data['celle']
+
+    def convert(ev: dict):
+        e = Event()
+        e.begin = datetime.fromtimestamp(
+            ev['timestamp'], tz=ROME)
+
+        # Ora fine se è tutto il giorno è 24:00 quindi non si riesce a parsare eg:: nel caso di chiusura_type
+        if ev['tipo'] == 'chiusura_type':
+            e.make_all_day()
+            e.name = ev['nome']
+        else:
+            ora_fine = ev['ora_fine'].split(':')
+            e.end = datetime.combine(e.begin.date(), time(
+                int(ora_fine[0]), int(ora_fine[1]), tzinfo=ROME), tzinfo=ROME)
+
+            if alarms:
+                e.alarms = [DisplayAlarm(-timedelta(hours=3)),
+                            DisplayAlarm(-timedelta(hours=2))]
+
+            e.name = f"{ev['nome_insegnamento']} in {ev['codice_aula']} con {ev['docente']}"
+        return e
+
+    c = Calendar()
+    c.events = [convert(ev) for ev in celle]
+
+    return Response(content=c.serialize(), media_type='text/calendar')
